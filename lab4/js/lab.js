6b0bb81fca29db2b4ea679caa343d1c24565e8e9
soniaelliss <ellissonia5@gmail.com>
2021-10-18 15:18:07 -0700

    lab 5 everything

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+// Lab 4 Javascript ART101.
+// Turning pseudocode into comments.
+// @link   URL
+// @file   This file contains my Lab 4 Javascript.
+// @author SoniaEllis.
+// @since  10.13.21
+  // Create rock, paper, and scissors icons for user to choose from
+    // Accept user input
+      // Validate user move
+      // Evaluate possible moves
+  // Choose computer move
+// Test of win or draw condition
---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+*Authors: Jocque Jefferson <jopjeffe@ucsc.edu> and
+Sonia Ellis <solellis@ucsc.edu>
+*
+*Created: 18 October
+*License: Public Domain
+*/
+
+// Define Variables
+make = "Chevrolet";
+model = "Corvair";
+color = "light yellow";
+year = "1965";
+
+//calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
